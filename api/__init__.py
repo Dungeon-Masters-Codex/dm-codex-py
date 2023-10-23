3841edfda64dcd39f513005438d64f905845cff8
Lando <landonlewisturner@gmail.com>
2023-10-23 18:21:25 -0400

    add flask migrate

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,7 @@
 from decouple import config
 from flask import Flask
 from flask_bcrypt import Bcrypt
-# from flask_migrate import Migrate # may just use alembic
+from flask_migrate import Migrate 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app.config.from_object(config("APP_SETTINGS"))
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
-# migrate = Migrate(app, db) # see comment above
+migrate = Migrate(app, db)
 
 # Registering blueprints
 from accounts.routes import accounts_bp
