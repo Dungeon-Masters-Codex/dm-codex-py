f21ae67f1d3cd4b16fb054d94617d0f3161f6d40
Lando <landonlewisturner@gmail.com>
2023-10-25 00:01:20 -0400

    update main file and user views

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-# TODO: after I get familiar with the flow here, break these out of main
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from typing import List, Annotated
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from database import db
+from fastapi import FastAPI
+from config import config
+import uvicorn
+import logging
 
-app = FastAPI()
 
-# TODO: found a little nudge on medium to cover gaps I had in the db stack:
-# https://python.plainenglish.io/fastapi-sqlalchemy-2-0-pydantic-v2-alembic-postgres-and-docker-2c429acfc333
+logger = logging.getLogger(__name__)
+
+API_PREFIX = "/api/v1"
+
+
+def init_app():
+    app = FastAPI(
+        title="Users App",
+        description="Handling Our User",
+        version="1",
+    )
+
+    # TODO: this is deprecated, update me
+    @app.on_event("startup")
+    def startup():
+        db.connect(config.DB_CONFIG)
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.disconnect()
+
+    # TODO: see if theres a cleaner file structure that I can pull all of these from one place
+    # maybe like api/__init__.py or routers.py and all the users, sessions, etc folders under the api dir
+    from users.views import user_api
+
+    app.include_router(
+        user_api,
+        prefix=API_PREFIX,
+    )
+
+    return app
+
+
+app = init_app()
--- users/views.py
+++ users/views.py
@@ -1,17 +1,63 @@
-from flask import Blueprint
-from flask_login import current_user, login_user
+from typing import List
 
-accounts_bp = Blueprint("accounts", __name__)
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 
-@accounts_bp.route("/register", methods=["GET", "POST"])
-def register():
-    if current_user.is_authenticated:
-        return "Update me this isn't what I would return to the FE"
+from users.models import User
+from database import db
 
-@accounts_bp.route("/login")
-def login():
-    pass
 
-@accounts_bp.route("/logout")
-def logout():
-    pass
+class UserSchema(BaseModel):
+    email: str
+    password: str
+    display_name: str
+
+
+
+class UserSerializer(BaseModel):
+    id: str
+    email: str
+    password: str
+    display_name: str
+
+
+    class Config:
+        from_attributes = True
+
+
+user_api = APIRouter(
+    prefix="/users",
+)
+
+
+@user_api.post("/")
+async def create_user(
+    user: UserSchema, db_session=Depends(db.get_db)
+) -> UserSerializer:
+    user = await User.create(db_session, **user.dict())
+    return user
+
+
+@user_api.get("/{id}")
+async def get_user(id: str, db_session=Depends(db.get_db)) -> UserSerializer:
+    user = await User.get(db_session, id)
+    return user
+
+
+@user_api.get("/")
+async def get_all_users(db_session=Depends(db.get_db)) -> List[UserSerializer]:
+    users = await User.get_all(db_session)
+    return users
+
+
+@user_api.put("/{id}")
+async def update(
+    id: str, user: UserSchema, db_session=Depends(db.get_db)
+) -> UserSerializer:
+    user = await User.update(db_session, id, **user.dict())
+    return user
+
+
+@user_api.delete("/{id}")
+async def delete_user(id: str, db_session=Depends(db.get_db)) -> bool:
+    return await User.delete(db_session, id)
