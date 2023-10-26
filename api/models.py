5168c62bfe389a0862e3d6c24d954b2ea9954a9b
Landon Turner <landonlewisturner@gmail.com>
2023-10-25 20:23:07 -0400

    setup docker-compose and begin implementing sqlalchemy

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class User(Base):
+    pass
+
+class Campaign(Base):
+    pass
