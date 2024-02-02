40d528a876aa4d472e30eb62a97af23cc867602a
landoDev <landonlewisturner@gmail.com>
2024-02-01 21:33:45 -0500

    add poetry security dependencies

--- api/auth/oauth.py
+++ api/auth/oauth.py
@@ -1,4 +1,17 @@
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+auth = APIRouter(prefix="/auth")
+
+@auth.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    # get username from form_data => form_data.username
+    # if not user dict, raise 400
+    # get hashed password
+    # if not password raise 400
+    # return access token (username) and token type (string bearer) 
+    pass
